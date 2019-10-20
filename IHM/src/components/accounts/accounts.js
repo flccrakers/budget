e8b4f9d75537c0f401ad82e620b214e44d841cf0
FLCcrakers <frank.hoonakker@gmail.com>
2019-10-21 01:42:34 +0200

    add select account and begin select year for account

--- IHM/src/components/accounts/accounts.js
+++ IHM/src/components/accounts/accounts.js
@@ -5,6 +5,8 @@ import {connect} from "react-redux";
 import {withRouter} from "react-router-dom";
 import {withStyles} from '@material-ui/core/styles';
 import {withSnackbar} from "notistack";
+import {CircularProgress, Select} from "@material-ui/core";
+import MenuItem from "@material-ui/core/MenuItem";
 
 
 const cardWidth = 200;
@@ -68,13 +70,19 @@ const styles = theme => ({
     flex: '1 1 auto',
     justifyContent: 'center',
     alignItems: 'center',
-    overflowY:'auto',
+    overflowY: 'auto',
 
   },
   tab: {maxHeight: '48px'},
   tabContainer: {
     display: 'flex',
     maxHeight: '48px'
+  },
+  overTab: {
+    display: 'flex',
+    flex: '0 0 auto',
+    padding: '15px',
+    alignItems:'center'
   }
 });
 
@@ -82,9 +90,9 @@ class Accounts extends Component {
 
   constructor(props) {
     super(props);
-    this.resourceUploadInputRef = undefined;
     this.state = {
-      selectedTab: 0
+      selectedTab: 0,
+      selectedAccount: 0,
     }
   }
 
@@ -99,12 +107,37 @@ class Accounts extends Component {
 
   getContent() {
     const {classes} = this.props;
-
+    let accounts = [{'name': '- Select an account -', 'id': '0'}].concat(this.props.accounts);
     return (
       <div className={classes.main}>
         <div className={classes.upperPart}>
-          {/*<div className={classes.tabContainer}>*/}
-          {/*  <AppBar position="static" style={{backgroundColor:'white', color:'black'}}>*/}
+          <div className={classes.overTab}>
+            {this.props.fetchingAccounts === true &&
+            <CircularProgress size={50} color={"secondary"}/>
+            }
+            {this.props.fetchingAccounts === false &&
+            <Select variant={"outlined"}
+                    value={this.state.selectedAccount}
+                    onChange={this.handleChangeAccount}
+                    style={{marginRight:'15px'}}
+            >
+              {accounts.map((account, index) => (
+                <MenuItem
+                  key={account.id}
+                  value={account.id}
+                >
+                  <div>
+                    {index === 0 && <em>{account.name}</em>}
+                    {index > 0 && account.name}
+                  </div>
+
+                </MenuItem>
+              ))}
+            </Select>
+            }
+            Select account
+            Select Year
+          </div>
           <Tabs value={this.state.selectedTab} onChange={this.handleChangeTab} aria-label="simple tabs example"
                 className={classes.tab}>
             <Tab label="January" id={1}/>
@@ -120,8 +153,6 @@ class Accounts extends Component {
             <Tab label="November" id={11}/>
             <Tab label="December" id={12}/>
           </Tabs>
-          {/*</div>*/}
-          {/*</AppBar>*/}
           <div className={classes.tabContent}>
             {'Content for tab ' + this.state.selectedTab}
           </div>
@@ -130,6 +161,9 @@ class Accounts extends Component {
     );
   }
 
+  handleChangeAccount = event => {
+    this.setState({selectedAccount: event.target.value})
+  }
   handleChangeTab = (event, newValue) => {
     console.log(newValue);
     this.setState({selectedTab: newValue})
